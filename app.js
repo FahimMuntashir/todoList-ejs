4cad23171f669e86a4ec4eee2034ca4c90fba1da
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-17 16:10:33 +0600

    adding new Items

--- app.js
+++ app.js
@@ -2,54 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+var item = "";
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday';
+    options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    };
 
-            break;
-        case 1:
-            day = 'Monday';
-
-            break;
-        case 2:
-            day = 'Tuesday';
-
-            break;
-        case 3:
-            day = 'Wednesday';
-
-            break;
-        case 4:
-            day = 'Thursday';
-
-            break;
-        case 5:
-            day = 'Friday';
-
-            break;
-        case 6:
-            day = 'Saturday';
-
-            break;
-
-        default:
-            console.log('something gone wrong');
-            
-    }
+    var day = today.toLocaleDateString('en-US', options);
 
 
-    res.render("list", { kindofDay: day });
+    res.render("list", { kindofDay: day, newListItem: item });
 
-})
+});
+
+app.post('/', function (req, res) {
+    item = req.body.newItem;
+    console.log(item);
+
+    res.redirect("/");
+
+});
+
+
 
 
 app.listen(3000, function (req, res) {
